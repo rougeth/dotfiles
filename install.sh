02415e0ce3a3adda18dd87b139b25ad678816bb9
Marco Rougeth <marco@rougeth.com>
2014-01-04 20:17:29 -0200

    Remove useless file and update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # I'm sure that exists a better way to do it
 sudo rm -r .git/ README.md
 profile_path=$HOME/.gconf/apps/gnome-terminal/profiles
-mv -f  $profile_path/Default $profile_path/.Default.backup
-mv -f gnome-terminal-profile $profile_path/Default
+cp -rf  $profile_path/Default $profile_path/.Default.backup
+cp -rf gnome-terminal-profile $profile_path/Default
+rm gnome-terminal-profile
 shopt -s dotglob
 cp -r * $HOME
