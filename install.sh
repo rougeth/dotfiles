5fffaf5cdfca46f59757e02878d71cebde8a277d
Marco Rougeth <marco@rougeth.com>
2014-01-12 20:56:20 -0200

    Add Powerline symbols

--- install.sh
+++ install.sh
@@ -47,6 +47,15 @@ ln -s ~/.vim/vimrc ~/.vimrc
 cd ~/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb && make
 
+# Configuring Powerline symbols
+cd /tmp
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/ 
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
 # -----------------------------------------------------------------------------
 # => Gnome-terminal settings
 # -----------------------------------------------------------------------------
